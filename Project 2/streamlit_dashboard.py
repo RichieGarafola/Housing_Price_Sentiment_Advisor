47a765c7b1ce50728f3cfd6ed307306b0aff50af
Richie Garafola <99199925+RichieGarafola@users.noreply.github.com>
2022-07-11 10:00:29 -0400

    Add files via upload

--- Project 2/streamlit_dashboard.py
+++ Project 2/streamlit_dashboard.py
@@ -4,6 +4,11 @@ import matplotlib.pyplot as plt # plots
 import pandas_datareader as pdr # Apis for our Data
 from pandas_datareader import data as wb # Apis for our Data
 import datetime as dt # Date time format
+from plotly import graph_objs as go
+
+# from fbprophet import Prophet
+# from fbprophet.plot import plot_plotly
+
 
 # dashboard title
 st.title("FRED House Price Index")
@@ -28,32 +33,23 @@ start = st.date_input('Start', value = pd.to_datetime('1960-01-01'))
 end = st.date_input('End', value = pd.to_datetime('today'))
 
 
-if len(dropdown) > 0:
-    df = wb.DataReader(dropdown,'fred',start,end)
-    df2 = wb.DataReader(dropdown2,'fred',start,end)
-    
-#     st.header('Returns of {}'.format(dropdown))
-#     st.line_chart(df)
-#     st.header('Returns of {}'.format(dropdown2))
-#     st.line_chart(df2)
-        
-    # Two equal columns:
-    col1, col2 = st.columns(2)
-    col1.line_chart(df)
-    col2.line_chart(df2)
-    
-    # Plots Overlayed
-    linear_sequence = df2
-    exponential_sequence = df
-    fig, ax = plt.subplots()
-    ax.plot(linear_sequence, color='red')
-    ax.tick_params(axis='y', labelcolor='red')
-    ax2 = ax.twinx()
-    ax2.plot(exponential_sequence, color='green')
-    ax2.tick_params(axis='y', labelcolor='green')
-    # plt.show()
-    st.plotly_chart(fig)
+df = wb.DataReader(dropdown,'fred',start,end) # State Index
+df2 = wb.DataReader(dropdown2,'fred',start,end) # Mortgage Rate
 
-    
-    
-    
+st.write("State Index (Left) Mortgage Rates (Right)")      
+# Two equal columns:
+col1, col2 = st.columns(2)
+col1.line_chart(df)
+col2.line_chart(df2)
+ 
+st.write("Plots Overlayed")    
+# Plots Overlayed
+linear_sequence = df2
+exponential_sequence = df
+fig, ax = plt.subplots()
+ax.plot(linear_sequence, color='red')
+ax.tick_params(axis='y', labelcolor='red')
+ax2 = ax.twinx()
+ax2.plot(exponential_sequence, color='green')
+ax2.tick_params(axis='y', labelcolor='green')
+st.pyplot(plt)
